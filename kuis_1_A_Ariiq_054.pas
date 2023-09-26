ac6b4f7c229bce765fa6181d3bbb683bd3297d6d
AriiqAlhafizh <145732072+AriiqAlhafizh@users.noreply.github.com>
2023-09-26 09:53:14 +0700

    kuis 1

---
+++ kuis_1_A_Ariiq_054.pas
@@ -0,0 +1,18 @@
+program kuis1_A;
+
+var
+a,b,c:string;
+e:real;
+
+begin
+    write('masukkan nama anda: ');
+    readln(a);
+    write('masukkan NIM anda: ');
+    readln(e);
+    write('masukkan KOM anda: ');
+    readln(b);
+    write('masukkan alamat anda:');
+    readln(c);
+    writeln;
+    writeln('halo, nama saya ',a,', NIM saya ',e:0:0,', saya di KOM  ',b,', alamat saya di ',c,'. salam kenal semuanya!' );
+end.
---
+++ kuis_1_B_Ariiq_054.pas
@@ -0,0 +1,18 @@
+program kuis1_B;
+
+var
+a,b:string;
+c,d:real;
+
+begin
+    write('jalan: ');
+    readln(a);
+    write('No: ');
+    readln(c);
+    write('kota: ');
+    readln(b);
+    write('Kode Pos: ');
+    readln(d);
+    writeln;
+    writeln('alamat: Jalan ',a,' No. ',c:0:0,' ',b,' ',d:0:0);
+end.
